050ede5ba6da37e85088e484abcaac8a09b90a2e
JeddySnm <126888709+JeddySnm@users.noreply.github.com>
2023-06-09 17:25:06 -0400

    actualizacion

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function mostrarImagen(event) {
     
     seccion.style.backgroundImage = 'url(' + canvas.toDataURL() + ')';
   }
+
+  function mostrarMenu() {
+    var menu = document.getElementById('menu');
+    
+    if (menu.style.display === 'none') {
+      menu.style.display = 'block'; // Mostrar el menú si está oculto
+    } else {
+      menu.style.display = 'none'; // Ocultar el menú si está visible
+    }
+  }
+
+  function mostrarMenuTransformar(){}
